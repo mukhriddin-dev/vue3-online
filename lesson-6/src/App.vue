65e5465d25aca810538035cc45b31e46924599e7
mukhriddin-dev <khodievm@gmail.com>
2024-11-20 21:39:52 +0500

    NEW:added lesson 6

---
+++ lesson-6/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>Life cycle methods!</h1>
+    <h1>Counter:{{ count }}</h1>
+    <h1>{{ num }}</h1>
+    <button @click="count++">Increment</button>
+    <button @click="IncrementNumber">IncrementNumber</button>
+    <button @click="toggleCompontent">Toggle Component</button>
+
+  </div>
+  <HelloWorld v-if="toggle" />
+</template>
+
+<script setup>
+import { onMounted, onBeforeMount , ref , onBeforeUpdate , onUpdated } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+
+let num = 1;
+const count = ref(1);
+const toggle = ref(true);
+
+
+const toggleCompontent = () => {
+  toggle.value =!toggle.value;
+}
+
+const IncrementNumber = () => {
+  num++;
+}
+
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+})
+
+onMounted(() => {
+  console.log("mounted");
+})
+
+onUpdated(() => {
+  console.log("updated");
+})
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate");
+})
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ lesson-6/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    Hello world!
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onBeforeMount, onBeforeUpdate, onUpdated , onBeforeUnmount , onUnmounted  } from "vue";
+
+
+
+
+onMounted(() => {
+  console.log("mounted H");
+})
+
+onUpdated(() => {
+  console.log("updated H");
+})
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate H");
+})
+
+
+onBeforeUnmount(() => { 
+  console.log("beforeUnmount H");
+})
+
+onUnmounted(() => { 
+  console.log("unmounted H");
+})
+
+onBeforeMount(() => {
+  console.log("onBeforeMount H");
+})
+
+</script>
+
+<style scoped>
+.card{
+  width: 400px;
+  height: 400px;
+  display: grid;
+  place-content: center;
+  background-color: #fee6e6;
+  margin: 50px;
+}
+</style>
